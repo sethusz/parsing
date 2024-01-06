07c9e16d55400a551e4d4349bc89f1b7f5558592
Oleksandr <oleksandrzelenjak@gmail.com>
2024-01-06 17:33:06 +0200

    fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({   headless: true,   args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
   await page.goto('https://google.com', { waitUntil: 'networkidle0' });
