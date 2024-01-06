5462af0cf8e4a49fa4c6464ebc019f4894f2edef
Oleksandr <oleksandrzelenjak@gmail.com>
2024-01-06 17:37:38 +0200

    fixed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer';
     const browser = await puppeteer.launch({   headless: true,   args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
-  await page.goto('https://google.com', { waitUntil: 'networkidle0' });
+  await page.goto('https://es.wallapop.com/app/search?category_ids=100&filters_source=quick_filters&latitude=40.41955&longitude=-3.69196&distance=50000&order_by=newest', { waitUntil: 'networkidle0' });
 
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
